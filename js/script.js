3f5b7cbeff76a5220d503aebf326a107f9c5aa9d
Halls06 <antoniomotoe4plus@gmail.com>
2025-08-13 12:55:38 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#Profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/logo-maravilhasvivi.png")
+  }else {
+    img.setAttribute("src", "./assets/maravilhas-da-vivi.png")
+  }
+}
